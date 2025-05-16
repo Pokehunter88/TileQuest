e534779ba49b0da3f786a0ca5d275b20b117c5bc
Pokehunter88 <camster41@icloud.com>
2025-05-15 23:25:13 -0400

    full movement testing mobile movement

--- js/main.js
+++ js/main.js
@@ -4,15 +4,13 @@ const ctx = canvas.getContext('2d');
 const images = {
     level1: new Image(),
     player: new Image(),
-    player1: new Image(),
     tile: new Image(),
     brokenTile: new Image(),
     keyTile: new Image(),
 }
 
 images.level1.src = 'assets/Level 1.png';
-images.player.src = 'assets/Player.png';
-images.player1.src = 'assets/Player1.png';
+images.player.src = 'assets/Player Sheet.png';
 images.tile.src = 'assets/Tile.png';
 images.brokenTile.src = 'assets/TileBroken.png';
 images.keyTile.src = 'assets/TileKey.png';
@@ -59,22 +57,56 @@ document.addEventListener("DOMContentLoaded", (event) => {
         ctx.drawImage(images.player, playerX, playerY);
 
         playerAnimation()
-    }, 200)
+    }, 20)
 });
 
 // document.documentElement.style.setProperty('--canvas-scale', '3');
 
+const keysPressed = {
+    w: false,
+    s: false,
+    a: false,
+    d: false
+};
+
+document.addEventListener("keydown", (event) => {
+    if (event.code === "KeyR") {
+        window.location.reload();
+    } else if (event.code === "KeyW") {
+        keysPressed.w = true;
+    } else if (event.code === "KeyS") {
+        keysPressed.s = true;
+    } else if (event.code === "KeyA") {
+        keysPressed.a = true;
+    } else if (event.code === "KeyD") {
+        keysPressed.d = true;
+    }
+});
+
+document.addEventListener("keyup", (event) => {
+    if (event.code === "KeyW") {
+        keysPressed.w = false;
+    } else if (event.code === "KeyS") {
+        keysPressed.s = false;
+    } else if (event.code === "KeyA") {
+        keysPressed.a = false;
+    } else if (event.code === "KeyD") {
+        keysPressed.d = false;
+    }
+});
+
 let frame = 0;
 let playerIdleState = true;
+let playerAnimationState = 0;
+
+let playerDirection = "none";
+
+const speed = 2;
+const playerOffset = 5;
 
 function playerAnimation() {
     ctx.drawImage(images.level1, 0, 0);
     drawTiles();
-    if (playerIdleState) {
-        ctx.drawImage(images.player, playerSpriteX, playerSpriteY);
-    } else {
-        ctx.drawImage(images.player1, playerSpriteX, playerSpriteY);
-    }
 
     frame++;
 
@@ -82,14 +114,137 @@ function playerAnimation() {
         playerIdleState = !playerIdleState;
     }
 
+    let moving = false;
+
     if (playerX > playerSpriteX) {
-        playerSpriteX++;
+        if (frame % speed == 0)
+            playerSpriteX++;
+        playerDirection = "right";
+        moving = true;
     } else if (playerX < playerSpriteX) {
-        playerSpriteX--;
+        if (frame % speed == 0)
+            playerSpriteX--;
+        playerDirection = "left";
+        moving = true;
     } else if (playerY > playerSpriteY) {
-        playerSpriteY++;
+        if (frame % speed == 0)
+            playerSpriteY++;
+        playerDirection = "down";
+        moving = true;
     } else if (playerY < playerSpriteY) {
-        playerSpriteY--;
+        if (frame % speed == 0)
+            playerSpriteY--;
+        playerDirection = "up";
+        moving = true;
+    }
+
+    if (moving) {
+        if (frame % 10 == 0) {
+            if (playerAnimationState == 3) {
+                playerAnimationState = 0;
+            } else {
+                playerAnimationState++;
+            }
+        }
+
+        switch (playerDirection) {
+            case "down":
+                if (playerAnimationState == 0 || playerAnimationState == 2) {
+                    ctx.drawImage(images.player, 32, 0, 16, 16, playerSpriteX, playerSpriteY - playerOffset, 16, 16);
+                } else {
+                    ctx.drawImage(images.player, 48, 0, 16, 16, playerSpriteX, playerSpriteY - playerOffset, 16, 16);
+                }
+                break;
+            case "up":
+                if (playerAnimationState == 0 || playerAnimationState == 2) {
+                    ctx.drawImage(images.player, 32, 16, 16, 16, playerSpriteX, playerSpriteY - playerOffset, 16, 16);
+                } else {
+                    ctx.drawImage(images.player, 48, 16, 16, 16, playerSpriteX, playerSpriteY - playerOffset, 16, 16);
+                }
+                break;
+            case "left":
+                if (playerAnimationState == 0) {
+                    ctx.drawImage(images.player, 0, 32, 16, 16, playerSpriteX, playerSpriteY - playerOffset, 16, 16);
+                } else if (playerAnimationState == 1) {
+                    ctx.drawImage(images.player, 16, 32, 16, 16, playerSpriteX, playerSpriteY - playerOffset, 16, 16);
+                } else if (playerAnimationState == 2) {
+                    ctx.drawImage(images.player, 0, 48, 16, 16, playerSpriteX, playerSpriteY - playerOffset, 16, 16);
+                } else {
+                    ctx.drawImage(images.player, 16, 48, 16, 16, playerSpriteX, playerSpriteY - playerOffset, 16, 16);
+                }
+                break;
+            case "right":
+                if (playerAnimationState == 0) {
+                    ctx.drawImage(images.player, 32, 32, 16, 16, playerSpriteX, playerSpriteY - playerOffset, 16, 16);
+                } else if (playerAnimationState == 1) {
+                    ctx.drawImage(images.player, 48, 32, 16, 16, playerSpriteX, playerSpriteY - playerOffset, 16, 16);
+                } else if (playerAnimationState == 2) {
+                    ctx.drawImage(images.player, 32, 48, 16, 16, playerSpriteX, playerSpriteY - playerOffset, 16, 16);
+                } else {
+                    ctx.drawImage(images.player, 48, 48, 16, 16, playerSpriteX, playerSpriteY - playerOffset, 16, 16);
+                }
+                break;
+        }
+    } else {
+        switch (playerDirection) {
+            case "none":
+                if (playerIdleState) {
+                    ctx.drawImage(images.player, 64, 32, 16, 16, playerSpriteX, playerSpriteY - playerOffset, 16, 16);
+                } else {
+                    ctx.drawImage(images.player, 80, 32, 16, 16, playerSpriteX, playerSpriteY - playerOffset, 16, 16);
+                }
+                break;
+            case "down":
+                if (playerIdleState) {
+                    ctx.drawImage(images.player, 0, 0, 16, 16, playerSpriteX, playerSpriteY - playerOffset, 16, 16);
+                } else {
+                    ctx.drawImage(images.player, 16, 0, 16, 16, playerSpriteX, playerSpriteY - playerOffset, 16, 16);
+                }
+                break;
+            case "up":
+                if (playerIdleState) {
+                    ctx.drawImage(images.player, 0, 16, 16, 16, playerSpriteX, playerSpriteY - playerOffset, 16, 16);
+                } else {
+                    ctx.drawImage(images.player, 16, 16, 16, 16, playerSpriteX, playerSpriteY - playerOffset, 16, 16);
+                }
+                break;
+            case "left":
+                if (playerIdleState) {
+                    ctx.drawImage(images.player, 64, 0, 16, 16, playerSpriteX, playerSpriteY - playerOffset, 16, 16);
+                } else {
+                    ctx.drawImage(images.player, 80, 0, 16, 16, playerSpriteX, playerSpriteY - playerOffset, 16, 16);
+                }
+                break;
+            case "right":
+                if (playerIdleState) {
+                    ctx.drawImage(images.player, 64, 16, 16, 16, playerSpriteX, playerSpriteY - playerOffset, 16, 16);
+                } else {
+                    ctx.drawImage(images.player, 80, 16, 16, 16, playerSpriteX, playerSpriteY - playerOffset, 16, 16);
+                }
+                break;
+        }
+
+        if (keysPressed.w && !keysPressed.s) {
+            move("up");
+        }
+        if (keysPressed.s && !keysPressed.w) {
+            move("down");
+        }
+        if (keysPressed.a && !keysPressed.d) {
+            move("left");
+        }
+        if (keysPressed.d && !keysPressed.a) {
+            move("right");
+        }
+
+        for (let y = 0; y < levelLayout.length; y++) {
+            for (let x = 0; x < levelLayout[y].length; x++) {
+                if (levelLayout[y][x] == 2) {
+                    levelLayout[y][x] = 5;
+                    console.log("changing");
+                }
+            }
+        }
     }
 
     requestAnimationFrame(playerAnimation);
@@ -98,9 +253,9 @@ function playerAnimation() {
 function drawTiles() {
     for (let y = 0; y < levelLayout.length; y++) {
         for (let x = 0; x < levelLayout[y].length; x++) {
-            if (levelLayout[y][x] == 1) {
+            if (levelLayout[y][x] == 1 || levelLayout[y][x] == 2) {
                 ctx.drawImage(images.tile, x * 16, y * 16);
-            } else if (levelLayout[y][x] == 2) {
+            } else if (levelLayout[y][x] == 5) {
                 ctx.drawImage(images.brokenTile, x * 16, y * 16);
             } else if (levelLayout[y][x] == 3) {
                 ctx.drawImage(images.keyTile, x * 16, y * 16);
@@ -113,98 +268,144 @@ function nextLevel() {
 
 }
 
-document.addEventListener("keypress", (event) => {
-    if (event.code === "KeyW") {
-        console.log("Up");
-        if (levelLayout[tileY - 1][tileX] == 1 || levelLayout[tileY - 1][tileX] == 3) {
-            if (levelLayout[tileY][tileX] == 1 || levelLayout[tileY][tileX] == 3) {
-                levelLayout[tileY][tileX] = 2;
+let startX, startY;
+
+element.addEventListener('touchstart', (event) => {
+  startX = event.touches[0].clientX;
+  startY = event.touches[0].clientY;
+  console.log("touch start");
+});
+
+element.addEventListener('touchend', (event) => {
+  const endX = event.changedTouches[0].clientX;
+  const endY = event.changedTouches[0].clientY;
+  const deltaX = endX - startX;
+  const deltaY = endY - startY;
+
+  // Check if it's a swipe (minimal distance threshold)
+  const swipeThreshold = 50;
+  if (Math.abs(deltaX) > swipeThreshold || Math.abs(deltaY) > swipeThreshold) {
+    // Determine swipe direction
+    if (Math.abs(deltaX) > Math.abs(deltaY)) {
+      // Horizontal swipe
+      if (deltaX > 0) {
+        console.log('Swiped right');
+        move("right");
+      } else {
+        console.log('Swiped left');
+        move("left");
+      }
+    } else {
+      // Vertical swipe
+      if (deltaY > 0) {
+        console.log('Swiped down');
+        move("down");
+      } else {
+        console.log('Swiped up');
+        move("up");
+      }
+    }
+  }
+});
+
+function move(direction) {
+    if (playerX == playerSpriteX && playerY == playerSpriteY) {
+        if (direction === "up") {
+            if (levelLayout[tileY - 1][tileX] == 1 || levelLayout[tileY - 1][tileX] == 3) {
+                if (levelLayout[tileY][tileX] == 1 || levelLayout[tileY][tileX] == 3) {
+                    levelLayout[tileY][tileX] = 2;
+                }
+
+                if (levelLayout[tileY - 1][tileX] == 3) {
+                    keys++;
+                }
+
+                playerY -= 16;
+                tileY--;
+            } else if (levelLayout[tileY - 1][tileX] == 4 && keys > 0) {
+                if (levelLayout[tileY][tileX] == 1 || levelLayout[tileY][tileX] == 3) {
+                    levelLayout[tileY][tileX] = 2;
+                }
+
+                playerY -= 16;
+                tileY--;
+
+                nextLevel();
+            } else {
+                playerDirection = "up"
             }
+        } else if (direction === "down") {
+            if (levelLayout[tileY + 1][tileX] == 1 || levelLayout[tileY + 1][tileX] == 3) {
+                if (levelLayout[tileY][tileX] == 1 || levelLayout[tileY][tileX] == 3) {
+                    levelLayout[tileY][tileX] = 2;
+                }
 
-            if (levelLayout[tileY - 1][tileX] == 3) {
-                keys++;
+                if (levelLayout[tileY + 1][tileX] == 3) {
+                    keys++;
+                }
+
+                playerY += 16;
+                tileY++;
+            } else if (levelLayout[tileY + 1][tileX] == 4 && keys > 0) {
+                if (levelLayout[tileY][tileX] == 1 || levelLayout[tileY][tileX] == 3) {
+                    levelLayout[tileY][tileX] = 2;
+                }
+
+                playerY += 16;
+                tileY++;
+
+                nextLevel();
+            } else {
+                playerDirection = "down"
             }
+        } else if (direction === "left") {
+            if (levelLayout[tileY][tileX - 1] == 1 || levelLayout[tileY][tileX - 1] == 3) {
+                if (levelLayout[tileY][tileX] == 1 || levelLayout[tileY][tileX] == 3) {
+                    levelLayout[tileY][tileX] = 2;
+                }
 
-            playerY -= 16;
-            tileY--;
-        } else if (levelLayout[tileY - 1][tileX] == 4 && keys > 0) {
-            if (levelLayout[tileY][tileX] == 1 || levelLayout[tileY][tileX] == 3) {
-                levelLayout[tileY][tileX] = 2;
+                if (levelLayout[tileY][tileX - 1] == 3) {
+                    keys++;
+                }
+
+                playerX -= 16;
+                tileX--;
+            } else if (levelLayout[tileY][tileX - 1] == 4 && keys > 0) {
+                if (levelLayout[tileY][tileX] == 1 || levelLayout[tileY][tileX] == 3) {
+                    levelLayout[tileY][tileX] = 2;
+                }
+
+                playerX -= 16;
+                tileX--;
+
+                nextLevel();
+            } else {
+                playerDirection = "left"
             }
+        } else if (direction === "right") {
+            if (levelLayout[tileY][tileX + 1] == 1 || levelLayout[tileY][tileX + 1] == 3) {
+                if (levelLayout[tileY][tileX] == 1 || levelLayout[tileY][tileX] == 3) {
+                    levelLayout[tileY][tileX] = 2;
+                }
 
-            playerY -= 16;
-            tileY--;
+                if (levelLayout[tileY][tileX + 1] == 3) {
+                    keys++;
+                }
 
-            nextLevel();
-        }
-    } else if (event.code === "KeyS") {
-        console.log("Down");
-        if (levelLayout[tileY + 1][tileX] == 1 || levelLayout[tileY + 1][tileX] == 3) {
-            if (levelLayout[tileY][tileX] == 1 || levelLayout[tileY][tileX] == 3) {
-                levelLayout[tileY][tileX] = 2;
+                playerX += 16;
+                tileX++;
+            } else if (levelLayout[tileY][tileX + 1] == 4 && keys > 0) {
+                if (levelLayout[tileY][tileX] == 1 || levelLayout[tileY][tileX] == 3) {
+                    levelLayout[tileY][tileX] = 2;
+                }
+
+                playerX += 16;
+                tileX++;
+
+                nextLevel();
+            } else {
+                playerDirection = "right"
             }
-
-            if (levelLayout[tileY + 1][tileX] == 3) {
-                keys++;
-            }
-
-            playerY += 16;
-            tileY++;
-        } else if (levelLayout[tileY + 1][tileX] == 4 && keys > 0) {
-            if (levelLayout[tileY][tileX] == 1 || levelLayout[tileY][tileX] == 3) {
-                levelLayout[tileY][tileX] = 2;
-            }
-
-            playerY += 16;
-            tileY++;
-
-            nextLevel();
-        }
-    } else if (event.code === "KeyA") {
-        console.log("Left");
-        if (levelLayout[tileY][tileX - 1] == 1 || levelLayout[tileY][tileX - 1] == 3) {
-            if (levelLayout[tileY][tileX] == 1 || levelLayout[tileY][tileX] == 3) {
-                levelLayout[tileY][tileX] = 2;
-            }
-
-            if (levelLayout[tileY][tileX - 1] == 3) {
-                keys++;
-            }
-
-            playerX -= 16;
-            tileX--;
-        } else if (levelLayout[tileY][tileX - 1] == 4 && keys > 0) {
-            if (levelLayout[tileY][tileX] == 1 || levelLayout[tileY][tileX] == 3) {
-                levelLayout[tileY][tileX] = 2;
-            }
-
-            playerX -= 16;
-            tileX--;
-
-            nextLevel();
-        }
-    } else if (event.code === "KeyD") {
-        console.log("Right");
-        if (levelLayout[tileY][tileX + 1] == 1 || levelLayout[tileY][tileX + 1] == 3) {
-            if (levelLayout[tileY][tileX] == 1 || levelLayout[tileY][tileX] == 3) {
-                levelLayout[tileY][tileX] = 2;
-            }
-
-            if (levelLayout[tileY][tileX + 1] == 3) {
-                keys++;
-            }
-
-            playerX += 16;
-            tileX++;
-        } else if (levelLayout[tileY][tileX + 1] == 4 && keys > 0) {
-            if (levelLayout[tileY][tileX] == 1 || levelLayout[tileY][tileX] == 3) {
-                levelLayout[tileY][tileX] = 2;
-            }
-
-            playerX += 16;
-            tileX++;
-
-            nextLevel();
         }
     }
-})
+}
