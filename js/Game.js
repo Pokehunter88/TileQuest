a6ddeab7b976491e81956934b817bae4b774e827
Pokehunter88 <camster41@icloud.com>
2025-06-20 02:42:48 +0100

    removed redundant code

--- js/Game.js
+++ js/Game.js
@@ -22,10 +22,10 @@ class Game {
 
         this.menuButton = document.getElementById("menu-button");
         this.menuButton?.addEventListener("click", () => {
-            if (!document.fullscreenElement) {
-                document.documentElement.requestFullscreen();
-                navigator.keyboard.lock(["Escape"]);
-            }
+            // if (!document.fullscreenElement) {
+            //     document.documentElement.requestFullscreen();
+            //     navigator.keyboard.lock(["Escape"]);
+            // }
 
             if (this.playing) {
                 this.pauseMenu.showing = !this.pauseMenu.showing;
@@ -33,38 +33,6 @@ class Game {
                 if (this.pauseMenu.showing) {
                     this.pauseMenu.selected = 0;
                 }
-
-                // if (
-                //     document.documentElement.style.getPropertyValue(
-                //         "--menu-visible"
-                //     ) == 0 ||
-                //     document.documentElement.style.getPropertyValue(
-                //         "--menu-visible"
-                //     ) === ""
-                // ) {
-                //     document.documentElement.style.setProperty(
-                //         "--menu-visible",
-                //         100
-                //     );
-                // } else {
-                //     document.documentElement.style.setProperty(
-                //         "--menu-visible",
-                //         0
-                //     );
-                // }
-            }
-        });
-
-        this.closeButton = document.getElementById("close-button");
-        this.closeButton?.addEventListener("click", () => {
-            if (
-                this.playing &&
-                document.documentElement.style.getPropertyValue(
-                    "--menu-visible"
-                ) == 100
-            ) {
-                document.documentElement.style.setProperty("--menu-visible", 0);
-                this.startScreen();
             }
         });
 
@@ -76,62 +44,7 @@ class Game {
         this.player.renderer = this.renderer;
         this.tiles = new Tiles(this.levels, this.ctx, this.renderer);
 
-        this.leftButton = document.getElementById("menu-left");
-        this.leftButton?.addEventListener("click", () => {
-            if (
-                document.documentElement.style.getPropertyValue(
-                    "--menu-visible"
-                ) == 100
-            ) {
-                if (this.levels.level == 0) {
-                    this.levels.level = this.levels.levels.length - 1;
-                } else {
-                    this.levels.level--;
-                }
-
-                this.restart();
-                document.documentElement.style.setProperty(
-                    "--canvas-scale",
-                    this.levels.getZoom(this.levels.level)
-                );
-
-                document.documentElement.style.setProperty("--menu-visible", 0);
-            }
-        });
-
-        this.rightButton = document.getElementById("menu-right");
-        this.rightButton?.addEventListener("click", () => {
-            if (
-                document.documentElement.style.getPropertyValue(
-                    "--menu-visible"
-                ) == 100
-            ) {
-                if (this.levels.level + 1 >= this.levels.levels.length) {
-                    this.levels.level = 0;
-                } else {
-                    this.levels.level++;
-                }
-
-                this.restart();
-                document.documentElement.style.setProperty(
-                    "--canvas-scale",
-                    this.levels.getZoom(this.levels.level)
-                );
-
-                document.documentElement.style.setProperty("--menu-visible", 0);
-            }
-        });
-
         this.lastFrame = Date.now();
-
-        // document.documentElement.style.setProperty(
-        //     "--canvas-scale",
-        //     this.levels.getZoom(this.levels.level)
-        // );
-
-        // setTimeout(() => {
-        //     document.documentElement.style.setProperty("--scale-speed", "1s");
-        // }, 100);
 
         const levelsToChange = this.levels;
 
@@ -146,8 +59,8 @@ class Game {
             .then((json) => {
                 levelsToChange.levels = json;
             })
-            .catch(function (error) {
-                console.log("Failed to fetch levels 1");
+            .catch(function () {
+                console.log("Failed to fetch levels using fallback");
                 fetch(
                     "https://raw.githubusercontent.com/Pokehunter88/TileQuest/refs/heads/main/js/levels.json"
                 )
@@ -162,7 +75,7 @@ class Game {
                         levelsToChange.levels = json;
                     })
                     .catch(function (error) {
-                        console.log("Failed to fetch levels 2");
+                        console.log("Failed to fetch levels using fallback");
                         console.log(error);
                     });
             });
